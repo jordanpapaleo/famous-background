0961663048cbefd15735f9d98c5aa8134ae7a051
Jordan Papaleo <papaleowebdev@gmail.com>
2015-07-22 12:10:12 -0700

    Created the section shimmer

--- src/app/index.js
+++ src/app/index.js
@@ -17,11 +17,23 @@ class App extends Node {
             .setMountPoint(0, 0)
             .setOrigin(0, 0);
 
+        this.model = {
+            hasShimmer: true
+        };
+
+        this.onReceive = (event, payload) => {
+            if(event === 'section-active') {
+                this.model.hasShimmer = !payload;
+            }
+        };
+
         this.renderTest2();
+
+        this.startShimmer();
     }
 
     renderTest2() {
-        let sections = [
+        this.sections = [
             new Node(), new Node(),
             new Node(), new Node()
         ];
@@ -33,8 +45,8 @@ class App extends Node {
             .setAlign(.5, .5)
             .setMountPoint(.5, .5);
 
-        for(let i = 0, j = sections.length; i < j; i++) {
-            let section = sections[i];
+        for(let i = 0, j = this.sections.length; i < j; i++) {
+            let section = this.sections[i];
             let mountPoint = [0, 0], backgroundPosition = 'left top';
 
             switch(i) {
@@ -75,18 +87,22 @@ class App extends Node {
             section.addUIEvent('mouseover');
             section.addUIEvent('mouseout');
 
-            section.onReceive = function(event, payload){
+            section.onReceive = (event, payload) => {
                 section.scale.halt();
 
                 if(event === 'mouseover'){
+                    section.emit('section-active', true);
                     section.domEl.setProperty('z-index', 6);
                     section.scale.set(1.1, 1.1, 1.1, {
-                        duration: 250
+                        duration: 250,
+                        curve: Curves.easeOutBounce
                     });
                 } else if(event === 'mouseout') {
                     section.scale.set(1, 1, 1, {
-                        duration: 250
+                        duration: 250,
+                        curve: Curves.easeOutBounce
                     }, () => {
+                        section.emit('section-active', false);
                         section.domEl.setProperty('z-index', 5);
                     });
                 }
@@ -98,72 +114,42 @@ class App extends Node {
         this.addChild(modifier);
     }
 
-    renderTest1() {
-        let modifier = new Node();
-        modifier
-            .setSizeMode('absolute', 'absolute', 'absolute')
-            .setAbsoluteSize(200, 200)
-            .setAlign(.5, .5)
-            .setMountPoint(.5, .5)
-            .setOrigin(.5, 0)
-            .domEl = new DOMElement(modifier, {
-                tagName: 'div',
-                properties: {
-                    'border': '1px solid black',
-                    'border-radius': '50%',
-                    'width': '200px',
-                    'height': '200px'
-                }
-        });
-
-        let front = new Node();
-        front
-            .setSizeMode('relative', 'absolute')
-            .setProportionalSize(1, null)
-            .setAbsoluteSize(null, 15)
-            .setAlign(.5, .5)
-            .setMountPoint(.5, .5)
-            .domEl = new DOMElement(front, {
-            tagName: 'div',
-            content: 'BUSYBEE',
-            properties: {
-                'backface-visibility': 'hidden',
-                'border': '1px solid blue',
-                'text-align': 'center'
+    startShimmer() {
+        if(this.model.hasShimmer) {
+            //var blar = this._shuffle(this.sections);
+            for(let i = 0, j = this.sections.length; i < j; i++) {
+                setTimeout(() => {
+                    this._shimmer(this.sections[i]);
+                }, Math.ceil(Math.random() * (1000 - 250) + 250));
             }
+            //console.log('blar',blar);
+        }
+    }
+
+    _shuffle(array) {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
+    _shimmer(section) {
+        section.scale.halt();
+        section.scale.set(1.05, 1.05, 1.05, {
+            duration: 200
+        }, () => {
+            section.scale.set(1, 1, 1, {
+                duration: 200
+            });
         });
-
-        let back = new Node();
-        back
-            .setSizeMode('relative', 'absolute')
-            .setProportionalSize(1, null)
-            .setAbsoluteSize(null, 15)
-            .setAlign(.5, .5)
-            .setMountPoint(.5, .5)
-            .domEl = new DOMElement(front, {
-            tagName: 'div',
-            content: 'EARs',
-            properties: {
-                'backface-visibility': 'hidden',
-                'border': '1px solid green',
-                'text-align': 'center'
-            }
-        });
-
-        back.rotation = new Rotation(back);
-        back.rotation.set(0, (Math.PI * 180) / 180, 0);
-
-        modifier.addChild(back);
-        modifier.addChild(front);
-        this.addChild(modifier);
-
-        modifier.rotation = new Rotation(modifier);
-        /*modifier.rotation.setY((Math.PI * 720) / 180, {
-         duration: 2000,
-         curve: Curves.inOutBack
-         }, () => {
-         console.log('here');
-         });*/
     }
 }
 
