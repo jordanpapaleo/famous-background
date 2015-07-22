e332e4d2043d39fef60508c6a0e68b52ee0eb88b
Jordan Papaleo <papaleowebdev@gmail.com>
2015-07-22 14:05:24 -0700

    Working

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,9 @@ const GestureHandler    = Famous.components.GestureHandler;
 const Curves            = Famous.transitions.Curves;
 const Node              = Famous.core.Node;
 const Rotation          = Famous.components.Rotation;
+const Position          = Famous.components.Position;
 const Scale             = Famous.components.Scale;
+const Size              = Famous.components.Size;
 
 class App extends Node {
     constructor() {
@@ -29,64 +31,66 @@ class App extends Node {
 
         this.renderTest2();
 
-        this.startShimmer();
+        this.initShimmer();
     }
 
     renderTest2() {
-        this.sections = [
-            new Node(), new Node(),
-            new Node(), new Node()
-        ];
+        var ratio = 7;
+
+        let width = 400;
+        let height = 400;
+
+        this.sections = [];
+
+        let nodeCount = ratio * ratio;
+        while(nodeCount) {
+            this.sections.push(new Node());
+            nodeCount--;
+        }
 
         let modifier = new Node();
         modifier
             .setSizeMode('absolute', 'absolute', 'absolute')
-            .setAbsoluteSize(200, 200)
+            .setAbsoluteSize(width, height)
             .setAlign(.5, .5)
             .setMountPoint(.5, .5);
 
-        for(let i = 0, j = this.sections.length; i < j; i++) {
-            let section = this.sections[i];
-            let mountPoint = [0, 0], backgroundPosition = 'left top';
 
-            switch(i) {
-                case 0:
-                    backgroundPosition = 'left top';
-                    mountPoint = [1, 1];
-                    break;
-                case 1:
-                    backgroundPosition = 'right top';
-                    mountPoint = [0, 1];
-                    break;
-                case 2:
-                    backgroundPosition = 'left bottom';
-                    mountPoint = [1, 0];
-                    break;
-                case 3:
-                    backgroundPosition = 'right bottom';
-                    mountPoint = [0, 0];
-                    break;
-            }
+        var size = { x: width / ratio, y: height / ratio};
+
+        for(let i = 0, j = this.sections.length; i < j; i++) {
+            var row = Math.floor(i / ratio);
+            var column = Math.floor(i % ratio);
+
+            let position = [row * size.x, column * size.y];
+            let section = this.sections[i];
 
             section
-                .setSizeMode('relative', 'relative')
-                .setProportionalSize(.5, .5)
-                .setAlign(.5, .5)
-                .setMountPoint(mountPoint[0], mountPoint[1])
-                .setOrigin(mountPoint[0], mountPoint[1]);
+                .setSizeMode('absolute', 'absolute')
+                .setAbsoluteSize(size.x, size.y)
+                .setAlign(0, 0)
+                .setMountPoint(0, 0)
+                .setOrigin(.5, .5);
+
+            section.position = new Position(section);
+            section.position.setX(position[0]);
+            section.position.setY(position[1]);
             section.scale = new Scale(section);
             section.domEl = new DOMElement(section, {
-                    tagName: 'div',
-                    classes: ['logo'],
-                    properties: {
-                        'background-position': backgroundPosition,
-                        'z-index': 5
-                    }
-                });
+                tagName: 'div',
+                classes: ['logo'],
+                properties: {
+                    'box-sizing': 'border-box',
+                    'background-position': `${-position[0]}px ${-position[1]}px`,
+                    'background-size': `${100 * ratio}% ${100 * ratio}%`,
+                    'border': '1px solid hotpink',
+                    'z-index': 5
+                }
+            });
 
+            //EVENTS
             section.addUIEvent('mouseover');
             section.addUIEvent('mouseout');
-
             section.onReceive = (event, payload) => {
                 section.scale.halt();
 
@@ -114,31 +118,14 @@ class App extends Node {
         this.addChild(modifier);
     }
 
-    startShimmer() {
-        if(this.model.hasShimmer) {
-            //var blar = this._shuffle(this.sections);
-            for(let i = 0, j = this.sections.length; i < j; i++) {
+    initShimmer() {
+        for(let i = 0, j = this.sections.length; i < j; i++) {
+            if(this.model.hasShimmer) {
                 setTimeout(() => {
                     this._shimmer(this.sections[i]);
                 }, Math.ceil(Math.random() * (1000 - 250) + 250));
             }
-            //console.log('blar',blar);
         }
-    }
-
-    _shuffle(array) {
-        let currentIndex = array.length, temporaryValue, randomIndex;
-
-        while (0 !== currentIndex) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-
-        return array;
     }
 
     _shimmer(section) {
